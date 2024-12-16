7d511da21334fc47258ac38643fb1515f80d359d
ladianchad <qhrejddlvltm@gmail.com>
2024-12-16 20:30:45 +0900

    updated word count
    Signed-off-by: ladianchad <qhrejddlvltm@gmail.com>

--- src/component/WritingArea.tsx
+++ src/component/WritingArea.tsx
@@ -1,18 +1,30 @@
+import {useState} from "react";
+
 const WritingArea = () => {
 
+    const [textCount, setTextCount] = useState(0)
+
     return (
-        <div className="flex flex-col">
-            <h4 className="text-xs">* 여기에 작성하세요.</h4>
+        <div className="flex flex-col grow">
+            <div className="flex justify-between items-end p-1 text-base font-semibold text-gray-500">
+                <span className="items-end">* 여기에 작성하세요.</span>
+                <span className="items-end">단어수 : {textCount}</span>
+            </div>
             <label
                 htmlFor="writingSection"
-                className="w-full border-2 border-gray-400 focus-within:border-gray-600 border-dashed"
+                className="w-full border-2 grow border-gray-400 focus-within:border-gray-400 border-dashed"
             >
             <textarea id="writingSection"
-                className="w-full h-full max-h-96 resize-y min-h-60 focus:outline-none focus:border-none p-2"
-                spellCheck="false"
-                autoComplete="off"
-                autoCorrect="off"
-                autoCapitalize="none"
+                      className="w-full h-full resize-y focus:outline-none focus:border-none p-2"
+                      spellCheck="false"
+                      autoComplete="off"
+                      autoCorrect="off"
+                      autoCapitalize="none"
+                      onChange={(event) => {
+                          const inputText = event.target.value;
+                          const words = inputText.trim().split(/\s+/).filter(Boolean);
+                          setTextCount(words.length)
+                      }}
             />
             </label>
         </div>
